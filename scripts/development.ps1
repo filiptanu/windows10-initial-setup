02b35950849a540b35399ba9038a0bde606acfb5
Filip Tanurovski <filiptanu@gmail.com>
2021-12-19 18:15:29 +0100

    Update development script

--- scripts/development.ps1
+++ scripts/development.ps1
@@ -1,6 +1,7 @@
+choco install androidstudio -y
 choco install docker-desktop -y
 choco install git -y
 choco install maven -y
-choco install nodejs --version=16.13.0 -y
+choco install nodejs-lts -y
 choco install oraclejdk
 choco install vscode -y
