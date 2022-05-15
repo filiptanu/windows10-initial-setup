1f27bf7948879eff6e41d1f041e5df323e34d7f5
Filip Tanurovski <filiptanu@gmail.com>
2022-05-15 22:38:17 +0200

    Update scripts

--- scripts/development.ps1
+++ scripts/development.ps1
@@ -1,7 +1,9 @@
+choco install anaconda3 -y
 choco install androidstudio -y
 choco install docker-desktop -y
 choco install git -y
+choco install intellijidea-community -y
+choco install jdk11 -y
 choco install maven -y
 choco install nodejs-lts -y
-choco install oraclejdk
 choco install vscode -y
--- scripts/essentials.ps1
+++ scripts/essentials.ps1
@@ -1,5 +1,7 @@
 choco install 7zip -y
 choco install firefox -y
+choco install megasync -y
 choco install picotorrent -y
+choco install sendtokindle -y
 choco install syncthing -y
 choco install vlc -y
