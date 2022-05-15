dc624130c67519934f6fe0e42dd4596104657ef4
Filip Tanurovski <filiptanu@gmail.com>
2022-05-15 22:47:03 +0200

    Update essentials script

--- scripts/essentials.ps1
+++ scripts/essentials.ps1
@@ -2,6 +2,9 @@ choco install 7zip -y
 choco install firefox -y
 choco install megasync -y
 choco install picotorrent -y
-choco install sendtokindle -y
+# choco install sendtokindle -y # The checksum did not match, install it manually for now
 choco install syncthing -y
 choco install vlc -y
+
+# Manual install
+# https://www.amazon.com/gp/sendtokindle
